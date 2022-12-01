71381f785ad9109461f47b8691094223ac4b8c0b
sk Lee <znfnfn2140@gmail.com>
2022-12-01 17:47:56 +0900

    이미지 테스트

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CanvasBox from './components/CanvasBox';
+
+function App() {
+  return (
+    <div className="App">
+      <CanvasBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CanvasBox.jsx
@@ -0,0 +1,12 @@
+const CanvasBox = () => {
+  return (
+    <div>
+      <h1>캔버스테스트</h1>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default CanvasBox;
